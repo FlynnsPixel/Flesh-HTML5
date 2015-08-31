0095b97b111385ee3579e44423329bdd18e4819d
FlynnsPixel <flynnspixel@gmail.com>
2015-09-01 01:59:01 +1000

    "Added phaser debug script"

--- src/test.js
+++ src/test.js
@@ -1,16 +1,31 @@
 /// <reference path="../phaser/phaser.d.ts" />
+/// <reference path="../phaser/phaser.d.ts" />
 window.onload = function () {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, null, { preload: preload, create: create, update: update });
+    var padding = 15;
+    var game = new Phaser.Game(window.innerWidth - padding, window.innerHeight - padding, Phaser.CANVAS, null, { preload: preload, create: create, update: update });
     var logo;
     function preload() {
-        game.load.image("logo", "phaser.png");
+        game.add.plugin(Phaser.Plugin.Debug);
+        game.load.image("logo", "assets/phaser.png");
     }
     function create() {
         logo = game.add.sprite(0, 0, "logo");
         logo.anchor.setTo(0.0, 0.0);
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.input.mouse.capture = true;
+        if (game.renderer instanceof PIXI.CanvasRenderer) {
+            logo.position.x = 40;
+        }
+        else {
+            logo.position.x = 400;
+        }
     }
     function update() {
-        logo.position.x = (Math.cos(game.time.now / 60.0) + 1) * 40.0;
+        if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            logo.position.x += .5;
+        }
+        if (game.input.activePointer.isDown) {
+            game.input.activePointer.position.copyTo(logo.position);
+        }
     }
 };
--- src/test.ts
+++ src/test.ts
@@ -1,22 +1,39 @@
 /// <reference path="../phaser/phaser.d.ts" />
+/// <reference path="../phaser/phaser.d.ts" />
 
 window.onload = function() {
 	
-	var game = new Phaser.Game(800, 600, Phaser.AUTO, null, { preload: preload, create: create, update:update });
+	var padding = 15;
+	var game = new Phaser.Game(window.innerWidth - padding, window.innerHeight - padding, Phaser.CANVAS, null, { preload: preload, create: create, update:update });
 	var logo: Phaser.Sprite;
+	var arr: Array<Phaser.Sprite> = [];
 	
 	function preload() {
-		game.load.image("logo", "phaser.png");
+		game.add.plugin(Phaser.Plugin.Debug);
+		
+		game.load.image("logo", "assets/phaser.png");
 	}
 	
 	function create() {
-		logo = game.add.sprite(0, 0, "logo");
-		logo.anchor.setTo(0.0, 0.0);
+		for (var n = 0; n < 20; ++n) {
+			var sprite = game.add.sprite(0, 0, "logo");
+			sprite.anchor.setTo(0.0, 0.0);
+			arr[n] = sprite;
+		}
 		
 		game.physics.startSystem(Phaser.Physics.ARCADE);
+		
+		game.input.mouse.capture = true;
+		
+		// if (game.renderer instanceof PIXI.CanvasRenderer) { }
 	}
 	
 	function update() {
-		logo.position.x = (Math.cos(game.time.now / 60.0) + 1) * 40.0;
+		if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+			logo.position.x += .5;	
+		}
+		if (game.input.activePointer.isDown) {
+			game.input.activePointer.position.copyTo(logo.position);
+		}
 	}
 };
