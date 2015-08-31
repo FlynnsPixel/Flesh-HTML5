c1ca620e432a0c5251a42af49ea33d312f1c8a44
FlynnsPixel <flynnspixel@gmail.com>
2015-09-01 02:40:10 +1000

    Spawned test squares that bounce off walls

--- src/test.js
+++ src/test.js
@@ -1,31 +1,50 @@
 /// <reference path="../phaser/phaser.d.ts" />
 /// <reference path="../phaser/phaser.d.ts" />
+var Square = (function () {
+    function Square() {
+    }
+    return Square;
+})();
+;
 window.onload = function () {
     var padding = 15;
     var game = new Phaser.Game(window.innerWidth - padding, window.innerHeight - padding, Phaser.CANVAS, null, { preload: preload, create: create, update: update });
     var logo;
+    var arr = [];
     function preload() {
         game.add.plugin(Phaser.Plugin.Debug);
-        game.load.image("logo", "assets/phaser.png");
+        game.load.image("square", "assets/square.png");
+    }
+    function spawn_squares(num) {
+        for (var n = 0; n < num; ++n) {
+            var square = new Square();
+            square.base = game.add.sprite(Math.random() * game.width, Math.random() * game.height, "square");
+            square.angle = Math.random() * Math.PI * 2.0;
+            square.base.anchor.setTo(0.0, 0.0);
+            arr.push(square);
+        }
     }
     function create() {
-        logo = game.add.sprite(0, 0, "logo");
-        logo.anchor.setTo(0.0, 0.0);
+        spawn_squares(1);
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.input.mouse.capture = true;
-        if (game.renderer instanceof PIXI.CanvasRenderer) {
-            logo.position.x = 40;
-        }
-        else {
-            logo.position.x = 400;
-        }
+        // if (game.renderer instanceof PIXI.CanvasRenderer) { }
     }
     function update() {
-        if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            logo.position.x += .5;
+        for (var n = 0; n < arr.length; ++n) {
+            var square = arr[n];
+            if (square.base.position.x < 0 || square.base.position.x > game.width - square.base.width) {
+                square.angle = -square.angle + Math.PI;
+            }
+            if (square.base.position.y < 0 || square.base.position.y > game.height - square.base.height) {
+                square.angle = -square.angle;
+            }
+            square.base.position.x += Math.cos(square.angle) * 4.0;
+            square.base.position.y += Math.sin(square.angle) * 4.0;
         }
+        //if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) { }
         if (game.input.activePointer.isDown) {
-            game.input.activePointer.position.copyTo(logo.position);
+            spawn_squares(5);
         }
     }
 };
--- src/test.ts
+++ src/test.ts
@@ -1,25 +1,37 @@
 /// <reference path="../phaser/phaser.d.ts" />
 /// <reference path="../phaser/phaser.d.ts" />
 
+class Square {
+	
+	base: Phaser.Sprite;
+	angle: number;
+};
+
 window.onload = function() {
 	
 	var padding = 15;
 	var game = new Phaser.Game(window.innerWidth - padding, window.innerHeight - padding, Phaser.CANVAS, null, { preload: preload, create: create, update:update });
 	var logo: Phaser.Sprite;
-	var arr: Array<Phaser.Sprite> = [];
+	var arr: Array<Square> = [];
 	
 	function preload() {
 		game.add.plugin(Phaser.Plugin.Debug);
 		
-		game.load.image("logo", "assets/phaser.png");
+		game.load.image("square", "assets/square.png");
+	}
+	
+	function spawn_squares(num: number) {
+		for (var n = 0; n < num; ++n) {
+			var square = new Square();
+			square.base = game.add.sprite(Math.random() * game.width, Math.random() * game.height, "square");
+			square.angle = Math.random() * Math.PI * 2.0;
+			square.base.anchor.setTo(0.0, 0.0);
+			arr.push(square);
+		}
 	}
 	
 	function create() {
-		for (var n = 0; n < 20; ++n) {
-			var sprite = game.add.sprite(0, 0, "logo");
-			sprite.anchor.setTo(0.0, 0.0);
-			arr[n] = sprite;
-		}
+		spawn_squares(1);
 		
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		
@@ -29,11 +41,21 @@ window.onload = function() {
 	}
 	
 	function update() {
-		if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-			logo.position.x += .5;	
+		for (var n = 0; n < arr.length; ++n) {
+			var square = arr[n];
+			if (square.base.position.x < 0 || square.base.position.x > game.width - square.base.width) {
+				square.angle = -square.angle + Math.PI;
+			}
+			if (square.base.position.y < 0 || square.base.position.y > game.height - square.base.height) {
+				square.angle = -square.angle;
+			}
+			square.base.position.x += Math.cos(square.angle) * 4.0;
+			square.base.position.y += Math.sin(square.angle) * 4.0;
 		}
+		
+		//if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) { }
 		if (game.input.activePointer.isDown) {
-			game.input.activePointer.position.copyTo(logo.position);
+			spawn_squares(5);
 		}
 	}
 };
