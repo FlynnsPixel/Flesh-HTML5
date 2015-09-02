36585750aeabc440a9f65d4a1a725706f5a91619
FlynnsPixel <flynnspixel@gmail.com>
2015-09-03 00:08:59 +1000

    Replaced phaser test with much faster raw pixi test

--- src/test.ts
+++ src/test.ts
@@ -1,98 +1,153 @@
-/// <reference path="../phaser/phaser.comments.d.ts" />
+/// <reference path="../pixi/pixi.js.d.ts" />
 
 class Square {
 	
-	base: Phaser.Sprite;
+	base: PIXI.Sprite;
 	angle: number;
 };
 
+var stage: PIXI.Container;
+var renderer: PIXI.CanvasRenderer | PIXI.WebGLRenderer;
+const padding = 15;
+var squares: Square[] = [];
+var square_tex: PIXI.Texture;
+var container: PIXI.ParticleContainer;
+var is_adding: boolean;
+
+window.onresize = function() {
+	resize_canvas();
+}
+function resize_canvas() {
+	var w = window.innerWidth - padding;
+	var h = window.innerHeight - padding;
+	renderer.view.style.width = w + "px";
+	renderer.view.style.height = h + "px";
+	renderer.resize(w, h);
+	console.log("resize: " + renderer.width + "x" + renderer.height);
+}
+
+function spawn_square(amount: number) {
+	for (var n = 0; n < amount; ++n) {
+		var square = new Square();
+		square.base = new PIXI.Sprite(square_tex);
+		
+		square.base.x = Math.random() * renderer.width;
+		square.base.y = Math.random() * renderer.height;
+		
+		square.angle = Math.random() * Math.PI * 2.0;
+		
+		square.base.filters = [new PIXI.filters.SepiaFilter()];
+		
+		stage.addChild(square.base);
+		squares.push(square);
+	}	
+}
+
 window.onload = function() {
+	// create a renderer instance
+	renderer = PIXI.autoDetectRenderer(400, 300);
+	renderer.backgroundColor = 0x99ff77;
+	document.body.appendChild(renderer.view);
 	
-	var padding = 15;
-	var game = new Phaser.Game(window.innerWidth - padding, window.innerHeight - padding, Phaser.CANVAS, null, { preload: preload, create: create, update: update });
-	var button: Phaser.Button;
-	var debug_text: Phaser.BitmapText;
-	var game_layer: Phaser.Group;
-	var ui_layer: Phaser.Group;
-	var batch: Phaser.SpriteBatch;
-	var cat: Phaser.Sprite;
-	var dest_game_zoom: number = 1;
-	const ZOOM_VALUE: number = .04;
-	
-	window.onresize = function() {
-		resize_game();
-	}
-	function resize_game() {
-		game.scale.setGameSize(window.innerWidth - padding, window.innerHeight - padding);
-		game.paused = false;
-		game.world.setBounds(-game.width * 2.0, -game.height * 2.0, game.width * 4.0, game.height * 4.0);
+	if (renderer instanceof PIXI.CanvasRenderer) {
+		console.log("using canvas renderer");
+	}else {
+		console.log("using webgl");
 	}
 	
-	function preload() {
-		game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
-		game.scale.forceOrientation(true, false);
-		game.scale.refresh();
-		
-		resize_game();
-		
-		//game.add.plugin(Phaser.Plugin.Debug);
-		
-		game.load.image("cat_base", "assets/chars/cat_base.png");
-		game.load.spritesheet("fullscreen_button", "assets/button_sprite_sheet.png", 193, 71);
-		game.load.bitmapFont("nokia", "assets/fonts/nokia16.png", "assets/fonts/nokia16.xml");
-	}
+	stage = new PIXI.Container();
+	container = new PIXI.ParticleContainer(100000, [false, true, false, false, false]);
+	stage.addChild(container);
 	
-	function create() {
-		batch = game.add.spriteBatch(null, "batch", true);
-		game_layer = game.add.group();
-		game_layer.z = 0;
-		ui_layer = game.add.group();
-		ui_layer.z = 1;
-		
-		button = new Phaser.Button(game, 0, 0, "fullscreen_button", function() {
-			game.scale.isFullScreen ? game.scale.stopFullScreen() : game.scale.startFullScreen();
-		}, this, 2, 1, 0);
-		ui_layer.add(button);
-		
-		debug_text = new Phaser.BitmapText(game, 0, 70, "nokia", "n/a");
-		ui_layer.add(debug_text);
-		game.time.advancedTiming = true;
-		
-		//game.physics.startSystem(Phaser.Physics.ARCADE);
-		
-		cat = new Phaser.Sprite(game, 200, 400, "cat_base");
-		game_layer.add(cat);
-		
-		//game.physics.startSystem(Phaser.Physics.P2JS);
-    	//game.physics.p2.enable(cat);
-		
-		game_layer.pivot.x = game.width * .5;
-		game_layer.pivot.y = game.height * .5;
-		game_layer.x = game.width * .5;
-		game_layer.y = game.height * .5;
-		
-		game.input.mouse.mouseWheelCallback = mouse_wheel_event;
-		function mouse_wheel_event(event) {
-			dest_game_zoom += game.input.mouse.wheelDelta * ZOOM_VALUE;
+	square_tex = PIXI.Texture.fromImage("assets/square.png");
+	
+	spawn_square(1);
+	resize_canvas();
+	game_loop();
+	
+	document.ontouchstart = mouse_down;
+	document.ontouchend = mouse_up;
+	document.onmousedown = mouse_down;
+	document.onmouseup = mouse_up;
+	
+	var vertices = new Float32Array(8);
+	vertices[0] = 0;
+	vertices[1] = 0;
+	vertices[2] = 100;
+	vertices[3] = 0;
+	vertices[4] = 100;
+	vertices[5] = 100;
+	vertices[6] = 0;
+	vertices[7] = 100;
+	var indices = new Uint16Array(4);
+	indices[0] = 0;
+	indices[1] = 1;
+	indices[2] = 2;
+	indices[3] = 3;
+	var uvs = new Float32Array(8);
+	uvs[0] = 0;
+	uvs[1] = 1;
+	uvs[2] = 1;
+	uvs[3] = 1;
+	uvs[4] = 1;
+	uvs[5] = 0;
+	uvs[6] = 0;
+	uvs[7] = 1;
+	var a = new PIXI.mesh.Mesh(square_tex);
+	a.vertices = vertices;
+	a.indices = indices;
+	a.uvs = uvs;
+	console.log(a.drawMode);
+	stage.addChild(a);
+}
+
+function mouse_down() {	
+	is_adding = true;
+}
+
+function mouse_up() {
+	is_adding = false;
+}
+
+setInterval(function() {
+	//console.log("fps: " + fps.getFPS() + ", squares: " + squares.length);
+}, 1000);
+
+var fps = {
+	startTime : 0,
+	frameNumber : 0,
+	getFPS : function(){
+		this.frameNumber++;
+		var d = new Date().getTime(),
+			currentTime = ( d - this.startTime ) / 1000,
+			result = Math.floor( ( this.frameNumber / currentTime ) );
+
+		if( currentTime > 1 ){
+			this.startTime = new Date().getTime();
+			this.frameNumber = 0;
 		}
-	}
-	
-	function update() {
-		debug_text.text = "fps: " + game.time.fps;
-		debug_text.update();
-		
-		if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-			//game.world.camera.position.x += 1;
-			cat.body.velocity.x = 400;
-		}
-		if (game.input.keyboard.isDown(Phaser.Keyboard.UNDERSCORE)) {
-			dest_game_zoom -= ZOOM_VALUE;
-		}
-		if (game.input.keyboard.isDown(Phaser.Keyboard.EQUALS)) {
-			dest_game_zoom += ZOOM_VALUE;
-		}
-		dest_game_zoom = Phaser.Math.clamp(dest_game_zoom, .2, 4.0);
-		game_layer.scale.x -= (game_layer.scale.x - dest_game_zoom) / 4.0;
-		game_layer.scale.y -= (game_layer.scale.y - dest_game_zoom) / 4.0;
-	}
+		return result;
+
+	}	
 };
+
+function game_loop() {
+	setTimeout(game_loop, 1000.0 / 60.0);
+	
+	fps.getFPS();
+	if (is_adding) spawn_square(100);
+	
+	for (var n = 0; n < squares.length; ++n) {
+		var square = squares[n];
+		if (square.base.position.x < 0 || square.base.position.x > renderer.width - square.base.width) {
+			square.angle = -square.angle + Math.PI;
+		}
+		if (square.base.position.y < 0 || square.base.position.y > renderer.height - square.base.height) {
+			square.angle = -square.angle;
+		}
+		square.base.position.x += Math.cos(square.angle) * 4.0;
+		square.base.position.y += Math.sin(square.angle) * 4.0;
+	}
+	
+	renderer.render(stage);
+}
